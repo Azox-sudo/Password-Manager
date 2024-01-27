fa869232a0504f6c50c690997d27a94cd3040902
azox-sudo <azox201150@gmail.com>
2024-01-27 13:50:17 +0000

    updated the config files

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,12 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        "theme-red": "#dc2626",
+        "theme-black": "#262626",
+        "theme-blue": "#0ea5e9",
+        "theme-white": "#fafafa",
+      },
     },
   },
   plugins: [],
